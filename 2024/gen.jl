5ec3ade77774eb350b1382cf2ba756fa2837b363
pere.carrillo.barrera <pere.carrillo.barrera@upc.edu>
2024-12-01 00:10:01 +0100

    Add generation script

---
+++ 2024/gen.jl
@@ -0,0 +1,36 @@
+using Dates, HTTP, JSON3, DotEnv
+
+#
+# To get the session cookie, open adventofcode.com -> f12 -> Application -> cookies
+#
+
+DotEnv.load!()
+
+const YEAR::Int = 2024
+
+if length(ARGS) == 1
+    day::Int = parse(Int, ARGS[1])
+else
+    day::Int = parse(Int, (rsplit(string(today()), "-", limit=2)[end]))
+end
+
+path = joinpath(pwd(), string(day))
+
+if !isdir(path)
+    mkpath(path)
+    touch(joinpath(path, "input.in"))
+    touch(joinpath(path, "test.in"))
+    touch(joinpath(path, "main.jl"))
+
+    cookies = Dict("session" => ENV["SESSION"])
+
+    resp = HTTP.get("https://adventofcode.com/$YEAR/day/$day/input", cookies=cookies)
+
+    data = resp
+
+    open(joinpath(path, "input.in"), "w") do file
+        write(file, data.body)
+    end
+
+end
+
